34e2c59c2ca096181ee2e692f9d86474a081ab35
SamuelNRyndem <111003352+SamuelNietoRyndem@users.noreply.github.com>
2024-11-21 07:55:09 -0600

    Actualizar environment.develop.ts

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://172.24.32.38:442/VentaDigital',
-  authApiUrl: 'https://172.24.32.38:9002',
+  authApiUrl: 'https://172.24.32.38:9001',
   fileBucket: 'mailbot'
 };
